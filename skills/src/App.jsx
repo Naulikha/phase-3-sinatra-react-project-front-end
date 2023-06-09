b08e36c46981281c4e4df459fe88573e14b07e55
Naulikha <nbiketia@gmail.com>
2023-06-09 21:15:23 +0300

    Complete front end

--- skills/src/App.jsx
+++ skills/src/App.jsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import logo from './logo.svg';
+import { React, useState } from "react";
+import "./App.css";
+import LogIn from "./components/login";
+import SignUp from "./components/signup";
+import HomePage from "./components/homepage";
+import Tasks from "./components/tasks";
+import SingleTask from "./components/singletask";
+import UpdateTask from "./components/updatetask";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [loginDetails, setLoginDetails] = useState({ name: "", password: "" });
+
+  let [signupDetails, setSignupDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  let [userID, setUserID] = useState(null);
+
+  let [task, setTask] = useState({
+    name: "",
+    description: "",
+    due: "",
+    status: "NOT STARTED",
+    userID: "",
+  });
+
+  let [allTasks, setAllTasks] = useState([]);
+
+  let [currTask, setcurrTask] = useState();
+
+  let [updatedStatus, setUpdatedStatus] = useState({ status: "" });
+
+  let [todayTasks, setTodayTasks] = useState(false);
+
+  let [filterValues, setFilterValues] = useState({ status: "ALL", due: "" });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <LogIn
+                loginDetails={loginDetails}
+                setLoginDetails={setLoginDetails}
+                userID={userID}
+                setUserID={setUserID}
+                setAllTasks={setAllTasks}
+              />
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <SignUp
+                signupDetails={signupDetails}
+                setSignupDetails={setSignupDetails}
+              />
+            }
+          />
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/tasks"
+            element={
+              <Tasks
+                setUserID={setUserID}
+                filterValues={filterValues}
+                setFilterValues={setFilterValues}
+                userID={userID}
+                task={task}
+                setTask={setTask}
+                allTasks={allTasks}
+                setAllTasks={setAllTasks}
+                currTask={currTask}
+                setcurrTask={setcurrTask}
+                todayTasks={todayTasks}
+                setTodayTasks={setTodayTasks}
+              />
+            }
+          />
+          <Route
+            path="/tasks/:id"
+            element={<SingleTask currTask={currTask} />}
+          />
+          <Route
+            path="/tasks/update/:id"
+            element={
+              <UpdateTask
+                setFilterValues={setFilterValues}
+                currTask={currTask}
+                userID={userID}
+                setAllTasks={setAllTasks}
+                updatedStatus={updatedStatus}
+                setUpdatedStatus={setUpdatedStatus}
+                setTodayTasks={setTodayTasks}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
